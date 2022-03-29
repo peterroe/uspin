4b831fec774e25497922fe1c5303d0087fbdd2b7
peterroe <peterroe@163.com>
2022-03-29 15:11:39 +0800

    ts support

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import dts from 'vite-plugin-dts'
 import pkg from './package.json'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), dts()],
   test: {
     environment: 'happy-dom',
   },
@@ -12,6 +13,7 @@ export default defineConfig({
       entry: './src/main.ts',
       formats: ['es', 'umd'],
       name: pkg.name,
+      fileName: format => `main.${format}.js`,
     },
     rollupOptions: {
       external: ['vue'],
